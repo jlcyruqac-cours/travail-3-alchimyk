254c6864f43f301f5d08fd3a416942f531f73d38
St-Pierre <Jean-SebaSt-Pierre@riotinto.com>
2019-10-08 19:27:19 -0400

    Ça s'en vient

--- app.py
+++ app.py
@@ -5,66 +5,121 @@
 # This is the logic implementation for the basic flask server
 
 from flask import Flask, request, render_template, jsonify
-import re, json
+import re, json, datetime
+from datetime import *
+import requests
 
 
 app = Flask(__name__)
 
 # Basic default route
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET'])
 def accueil():
-	titre = "Les astres chuchottent à votre oreille!"
+	titre = "Les astres chuchottent à votre oreille!"	
+	return render_template('index.html', titre = titre)
 	
-	# Data processing
-	if request.method == 'POST':
-		print("beep")
-		name = request.form.get('inputName')
-		fstName = request.form.get('inputFstName')
-		bthDate = request.form.get('inputDOB')
-        #Regex server-side validation for alphanumeric-only user input
-		# namePattern = "[a-zA-Z0-9- ]+$"
-		# bthdatePattern = "^((0|1)\\d{1})/((0|1|2|3)\\d{1})/((19|20)\\d{2})"
-		# if not re.match(pattern, answer):
-		# 	return render_template('noWay.tpl', titre = "Nooooooo!", message = "Tu m'auras pas comme ça!!  ;)")
-		# hashed = hashThisShit(answer, mode)
-		# if hashed == secretHash:
-		# 	verdict = "correspond"
-		# else:
-		# 	verdict = "ne correspond pas"
-			
-		return render_template('index.html', titre = titre, result = name, mode = fstName, verdict = bthDate)
-		
-	else :
-		return render_template('index.html', titre = titre)
-
 
 @app.route('/horoscope', methods = ['GET', 'POST'])
 def horoscope():
-	titre = "Écoutez le chant des étoiles"
 	
 	# Data processing
 	if request.method == 'POST':
 		name = request.form['inputName']
 		fstName = request.form['inputFstName']
 		bthDate = request.form['inputDOB']
-        #Regex server-side validation for alphanumeric-only user input
-		# namePattern = "[a-zA-Z0-9- ]+$"
-		# bthdatePattern = "^((0|1)\\d{1})/((0|1|2|3)\\d{1})/((19|20)\\d{2})"
-		# if not re.match(pattern, answer):
-		# 	return render_template('noWay.tpl', titre = "Nooooooo!", message = "Tu m'auras pas comme ça!!  ;)")
-		# hashed = hashThisShit(answer, mode)
-		# if hashed == secretHash:
-		# 	verdict = "correspond"
-		# else:
-		# 	verdict = "ne correspond pas"
-		#return jsonify(tite = titre, result = request.form['inputName'], mode = request.form['inputFstName'], verdict = request.form['inputDOB'] )
-		#test = jsonify(titre = titre, fstName = fstName)
+		validated = (validateInput(name, fstName, bthDate))
+		print(validated)
+		mysign = getSign(bthDate)
+		print(mysign)
+		requestHoroscope = getAztro(mysign)
+		myHoroscope = format(requestHoroscope.json()["description"])
 		
-		return json.dumps({'titre':titre, 'name':name, 'fstName': fstName, 'bthDate': bthDate})
+		return json.dumps({'name':name, 'fstName': fstName, 'bthDate': bthDate,
+		 'ErrorCodes': validated, 'sign': mysign, 'horoscope': myHoroscope})
 		
 	else :
-		return render_template('index.html', titre = "Fuck you all")
+		return render_template('index.html', titre = titre)
 
+
+def validateInput(name, fname, bdate):
+	#Regex server-side validation for alphanumeric-only user input
+	namePattern = "[a-zA-Z- ]+$"
+	bdatePattern = "^((0|1)\\d{1})/((0|1|2|3)\\d{1})/((19|20)\\d{2})"
+	validationError = []
+
+	if not name:
+		validationError.append("Le champ 'nom' est vide!\n")
+	else:
+		if not re.match(namePattern, name):
+			validationError.append(name + " est un nom invalide!\n")
+
+	if not fname:
+		validationError.append("Le champ 'prénom' est vide!\n")
+	else:
+		if not re.match(namePattern, fname):
+			validationError.append(fname + " est un nom invalide!\n")
+
+	if not bdate:
+		validationError.append("Le champ 'date de naissance' est vide!\n")
+	else:
+		m, d, y = bdate.split('/')
+		dateObj = date(int(y), int(m), int(d))
+		if dateObj > date.today():
+			validationError.append("Vous ne pouvez pas être né dans le futur\n")
+
+		elif not re.match(bdatePattern, bdate):
+			validationError.append(bdate + " est une date de naissance invalide!\n")
+
+	return validationError
+
+def getSign(date):
+	month, d, y = date.split('/')
+	day = int(d)
+	if month == '12': 
+		astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+
+	elif month == '1': 
+		astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+
+	elif month == '2': 
+		astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+
+	elif month == '3': 
+		astro_sign = 'Pisces' if (day < 21) else 'aries'
+
+	elif month == '4': 
+		astro_sign = 'Aries' if (day < 20) else 'taurus'
+
+	elif month == '5': 
+		astro_sign = 'Taurus' if (day < 21) else 'gemini'
+
+	elif month == '6': 
+		astro_sign = 'Gemini' if (day < 21) else 'cancer'
+
+	elif month == '7': 
+		astro_sign = 'Cancer' if (day < 23) else 'leo'
+
+	elif month == '8': 
+		astro_sign = 'Leo' if (day < 23) else 'virgo'
+
+	elif month == '9': 
+		astro_sign = 'Virgo' if (day < 23) else 'libra'
+
+	elif month == '10': 
+		astro_sign = 'Libra' if (day < 23) else 'scorpio'
+
+	elif month == '11': 
+		astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+
+	return astro_sign
+
+def getAztro(asign):
+	params = (
+		('sign', asign),
+		('day', 'today'),
+		)
+
+	return requests.post('https://aztro.sameerkumar.website/', params=params)
 
 # Error 404 management for any invalid GET request
 @app.errorhandler(404)
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,23 +1,40 @@
 $(document).ready(function()
 {
 	$( "#inputDOB" ).datepicker();
+	/*$("#response").hide();*/
 
-	/*$("#getHoroscope").click(function()
+
+	$("button").click(function(event)
 	{
-	    $.post("./horoscope", /*$("inputName"), $("inputFstName"), $("inputDOB"), 
-	    	{
-	    		name: $("#inputName"),
-		    	firstname: $("#inputFstName"),
-		    	dbirth: $("#inputDOB")
-		    },
-	    function(data,status)
-	    {
-	      alert("Data: " + data + "\nStatus: " + status);
-	    });
-	});*/
+		event.preventDefault();
+
+	    // Serialize the data in the form
+	    var serializedData = $("#bullshitRequestForm").serialize();
+
+	    $.post("./horoscope", serializedData,
+		    function(data,status)
+		    {
+		      console.log("Data:" + data + "\nStatus: " + status);
+		      var obj = JSON.parse(data);
+		      if ((obj.ErrorCodes).length > 0)
+		      {
+		      	/*(obj.ErrorCodes).join("");*/
+		      	window.alert((obj.ErrorCodes).join(""));
+		      	return;
+		      }
+		      window.alert(obj)
+		      $("#myform").toggle();
+		      $("#response").toggle();
+		      $("#theSign").html(obj.sign);
+		      $("#theName").html(obj.name);
+		      $("#theFirstName").html(obj.fstName);
+		      $("#theDOB").html(obj.bthDate);
+		      $("#thePred").html(obj.horoscope);
+		    });
+	});
 });
 
-$("button").click(function(event)
+/*$("button").click(function(event)
 {
 	event.preventDefault();
 
@@ -27,19 +44,18 @@ $("button").click(function(event)
     $.post("./horoscope", serializedData,
 	    function(data,status)
 	    {
-	      alert("data:" + data + "\nStatus: " + status);
+	      console.log("Data:" + data + "\nStatus: " + status);
 	      var obj = JSON.parse(data);
-	      window.alert(obj);
-	      $("#theName").html(obj.name)
-	      $("#theFirstName").html(obj.fstName)
-	      $("#theDOB").html(obj.bthDate)
-
-	    /*$("#titre").html = data.titre;*/
+	      if ((obj.ErrorCodes).length > 0)
+	      {
+	      	/*(obj.ErrorCodes).join("");
+	      	window.alert((obj.ErrorCodes).join(""));
+	      	return;
+	      }
+	      $("#response").toggle();
+	      $("#theTitre").html(obj.titre);
+	      $("#theName").html(obj.name);
+	      $("#theFirstName").html(obj.fstName);
+	      $("#theDOB").html(obj.bthDate);
 	    });
-	    /*function(responseText)
-	    {
-	    	window.alert(responseText)
-	    	var parsed_data = JSON.parse(responseText);
-	    	$('#result').html(JSON.stringify(parsed_data));
-	    });*/
-});
+});*/
